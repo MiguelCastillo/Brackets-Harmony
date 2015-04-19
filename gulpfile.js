4c72c6988dc2d4ff2cda4dad7b8ac9d18c5e6ca8
Miguel Castillo <manchagnu@gmail.com>
2015-04-19 09:36:01 -0400

    added proper handling for css and package.json watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
-var source     = require("vinyl-source-stream");
-var gulp       = require('gulp');
-var gulputil   = require('gulp-util');
-var zip        = require('gulp-zip');
-var size       = require('gulp-size');
-var buffer     = require('gulp-buffer');
-var merge      = require('merge-stream');
-var webserver  = require('gulp-webserver');
-var watchify   = require('watchify');
+var source = require("vinyl-source-stream");
+var gulp = require('gulp');
+var gulputil = require('gulp-util');
+var zip = require('gulp-zip');
+var size = require('gulp-size');
+var buffer = require('gulp-buffer');
+var merge = require('merge-stream');
+var webserver = require('gulp-webserver');
+var watchify = require('watchify');
 var browserify = require('browserify');
-var babelify   = require('babelify');
-var del        = require('del');
+var babelify = require('babelify');
+var del = require('del');
 
 
 // Base tasks
 gulp.task('default', ['build']);
 gulp.task('package', ['clean'], buildPackage);
-gulp.task('watch',   ['build'], buildWatcher);
-gulp.task('build',   ['clean'], buildAll);
-gulp.task('serve',   ['watch'], serve);
-gulp.task('clean',   clean);
+gulp.task('watch', ['build'], buildWatcher);
+gulp.task('build', ['clean'], buildAll);
+gulp.task('serve', ['watch'], serve);
+gulp.task('clean', clean);
+
+// Subtasks
+gulp.task('build-js', buildJS);
+gulp.task('build-style', buildStyle);
+gulp.task('build-package-json', buildPackageJSON);
 
 
 function clean(cb) {
@@ -29,7 +34,7 @@ function clean(cb) {
 function buildAll() {
   return getAllBuildStreams()
     .pipe(buffer())
-    .pipe(size({gzip: true}))
+    .pipe(size({title: 'build all', gzip: true}))
     .pipe(gulp.dest('dist'));
 }
 
@@ -38,7 +43,27 @@ function buildPackage() {
   return getAllBuildStreams()
     .pipe(buffer())
     .pipe(zip('release.zip'))
-    .pipe(size())
+    .pipe(size({title: 'package all'}))
+    .pipe(gulp.dest('dist'));
+}
+
+
+function buildJS() {
+  var runner = runnerJSBundler(createJSBundler());
+  return runner();
+}
+
+
+function buildStyle() {
+  return gulp.src('src/**/*.css')
+    .pipe(size({title: 'style', gzip: true}))
+    .pipe(gulp.dest('dist'));
+}
+
+
+function buildPackageJSON() {
+  return gulp.src('package.json')
+    .pipe(size({title: 'package.json', gzip: true}))
     .pipe(gulp.dest('dist'));
 }
 
@@ -46,19 +71,19 @@ function buildPackage() {
 function buildWatcher() {
   // Watch JS
   var bundler = watchify(createJSBundler());
-  var runner  = bundlerRunner(bundler);
+  var runner = runnerJSBundler(bundler);
   runner();
 
   bundler
     .on('update', runner)
     .on('log', gulputil.log);
 
-  // Watch CSS
-  gulp.watch('src/**/*.css', buildStyle);
+  gulp.watch('src/**/*.css', ['build-style']);
+  gulp.watch('package.json', ['build-package-json']);
 }
 
 
-function bundlerRunner(bundler) {
+function runnerJSBundler(bundler) {
   return function runBundler() {
     return bundler
       .bundle()
@@ -70,35 +95,21 @@ function bundlerRunner(bundler) {
 
 function createJSBundler() {
   return browserify({
-    debug: true,
-    basedir: 'src',
-    extensions: ['jsx']
-  })
-  .transform(babelify.configure({
-    optional: ["reactCompat"]
-  }))
-  .require('./main', {entry: true});
+      debug: true,
+      basedir: 'src',
+      extensions: ['jsx']
+    })
+    .transform(babelify.configure({
+      optional: ["reactCompat"]
+    }))
+    .require('./main', {
+      entry: true
+    });
 }
 
 
 function getAllBuildStreams() {
-  return merge([buildPackageJSON(), buildJS(), buildStyle()]);
-}
-
-
-function buildJS() {
-  var runner = bundlerRunner(createJSBundler());
-  return runner();
-}
-
-
-function buildStyle() {
-  return gulp.src('src/**/*.css');
-}
-
-
-function buildPackageJSON() {
-  return gulp.src('package.json');
+  return merge(buildPackageJSON(), buildJS(), buildStyle());
 }
 
 
