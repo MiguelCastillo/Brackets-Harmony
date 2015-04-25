bb9eebe21032ed6b59e72ea32c289837231d5fd3
Miguel Castillo <manchagnu@gmail.com>
2015-04-25 12:41:54 -0400

    Moved around the appReady module to keep it out of user code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // Brackets modules
 var React = brackets.getModule("thirdparty/react");
 
-import appReady from './appReady';
+// App ready helper
+import appReady from 'appReady';
+
+// Views... Views... Views...
 import MainView from './view/main.jsx';
 
 
+// Load things up when the app is ready
 appReady(() => {
   // Your ready callback is when the application is ready
   var container = $("<div id='harmony-container'>").appendTo('.main-view > .content')[0];
